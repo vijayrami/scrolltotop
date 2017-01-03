edb97be735c7036006bbfff0e9d80c249e9373d5
Vijay Rami <vijaymrami@gmail.com>
2017-01-03 18:51:37 +0530

    Update Page.php

--- app/code/local/Vijay/ScrollToTop/Helper/Page.php
+++ app/code/local/Vijay/ScrollToTop/Helper/Page.php
@@ -29,6 +29,7 @@ class Vijay_ScrollToTop_Helper_Page extends Mage_Core_Helper_Data
         $allowedActionNameList = array(
             'catalog_category_default',
             'catalog_category_layered',
+            'catalog_category_view',
         );
         $category = Mage::registry('current_category');
         return in_array(Mage::app()->getFrontController()->getAction()->getFullActionName(), $allowedActionNameList) && $category && $category->getId();
